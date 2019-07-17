b8cf7cade09c538d75e41b7c6a8578e983d1a1bb
Caroline <38522055+carolinechoi@users.noreply.github.com>
2019-07-16 21:33:00 -0500

    created starter vue app

---
+++ src/components/Map.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="main">
+        <p>This should give you a map of some sort.</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Map',
+    components: {
+    }
+  }  
+</script>
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from '../src/App.vue';
+
+let vm = new Vue({
+    el: '#app',
+    template: '<App/>',
+    components: {
+        App
+    },
+    data: {
+        message: 'Hello Vue!'
+    }
+})
